0f369fe4ebec3f72ca85d6784213c26b7072096b
smuser90 <sam.musso90@gmail.com>
2016-06-14 21:54:10 -0600

    I fixed it. Maybe.

--- server.js
+++ server.js
@@ -40,7 +40,7 @@ var locationLookup = function(ipAddress){
 var getIP = function(){
   console.log("Retreiving IP Address...");
   request("http://ipinfo.io/json", function(error, response, body) {
-    io.emit('pulse', response);
+    io.emit('pulse', response.body);
   });
 };
 
